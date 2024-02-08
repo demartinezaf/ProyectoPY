1b7658efeeba671b34a5a955f38d5c9f6699b56b
Your Name <demartinezaf@gmail.com>
2024-02-07 20:32:49 -0500

    tipos de variables

---
+++ primeraclase/Variables.py
@@ -0,0 +1,18 @@
+edad = 28
+nombre = "David"
+estatura = 1.75
+pizza = True
+json_b = {
+    "edad": edad,
+    "sexo": "M"
+}
+lista = ["nombre","edad","apellido"]
+
+r = type(edad)
+z = type(nombre)
+a = type(estatura)
+s = type(pizza)
+f = type(json_b)
+g = type(lista)
+
+print(r,z,a,s,f,g)
